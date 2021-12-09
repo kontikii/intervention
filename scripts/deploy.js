050940930b56a993319f6d2a3d69ec5e197b2666
wanapagan <dick.cheney@tutanota.com>
2021-12-09 16:25:09 +0200

    birth this monstrosity to life!

---
+++ scripts/deploy.js
@@ -0,0 +1,32 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+  const [deployer] = await hre.ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+
+
+  // We get the contract to deploy
+  const contract = await hre.ethers.getContractFactory("tokenSwap");
+  const swapper = await contract.deploy();
+
+  await swapper.deployed();
+
+  console.log("Greeter deployed to:", swapper.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
